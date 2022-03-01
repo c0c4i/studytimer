69eb9ac3c90ba0349011d5dadedf5a69825f41f3
c0c4i <samuele.besoli.sb@gmail.com>
2022-03-01 20:04:03 +0100

    chore: add poppins font

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,32 @@ import 'dart:io';
 import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_acrylic/flutter_acrylic.dart';
+import 'package:window_manager/window_manager.dart';
 
 import 'countdown.dart';
 import 'utils/is_desktop.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (isDesktop()) {
+
+  if (isDesktop) {
     await Window.initialize();
-    await Window.disableFullSizeContentView();
-    await Window.hideZoomButton();
-    await Window.disableMiniaturizeButton();
-    await Window.hideTitle();
-    await Window.makeTitlebarTransparent();
-    await DesktopWindow.setMaxWindowSize(const Size(360, 256));
-    await DesktopWindow.setMinWindowSize(const Size(360, 256));
+    await windowManager.ensureInitialized();
+    // await windowManager.setTitleBarStyle('hidden');
+    if (Platform.isMacOS) {
+      await Window.disableFullSizeContentView();
+      await Window.disableMiniaturizeButton();
+      await Window.hideZoomButton();
+      await Window.hideTitle();
+      await Window.makeTitlebarTransparent();
+    }
+
+    if (Platform.isWindows) {
+      // await Window.hideWindowControls();
+    }
+
+    await DesktopWindow.setMaxWindowSize(const Size(360, 264));
+    await DesktopWindow.setMinWindowSize(const Size(360, 264));
   }
 
   runApp(const MyApp());
@@ -31,9 +42,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Study Timer',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blueGrey, fontFamily: 'Poppins'),
       home: const CountDownTimerPage(),
     );
   }
